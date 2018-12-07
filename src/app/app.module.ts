bdbb6f4d274817e4bf6d091b5e20b2603c8c3666
Brenden Seidel <brenseid@gmail.com>
2018-12-07 17:12:31 -0500

    clean it up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+// Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
+// Custom Modules
+import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
